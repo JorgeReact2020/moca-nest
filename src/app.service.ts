84ffce22f34d0e63177a2b9539ba265e00b1afb5
Jorge Oliveira <joliveira@parkour3.com>
2025-11-18 22:48:02 -0500

    test response

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { json } from 'stream/consumers';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World2!';
+  getHello(): object {
+    return {
+      statusCode: 200,
+      message: 'Hello World2!',
+      data: {
+        timestamp: new Date().toISOString(),
+      }
+    };
   }
 }
