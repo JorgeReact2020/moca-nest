56ec6e86693aef1f1c2f95bd1d1f59babd8cbc76
Jorge Oliveira <joliveira@parkour3.com>
2026-01-08 12:38:10 -0500

    fix lint

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,7 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { request } from 'https';
 
 describe('AppController', () => {
   let appController: AppController;
