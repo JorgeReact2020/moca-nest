b430a856ab6fda484ae49fd44835007aab4c703d
Jorge Oliveira <joliveira@parkour3.com>
2026-01-08 14:53:50 -0500

    fixe CLI orm

--- src/typeorm.config.ts
+++ src/typeorm.config.ts
@@ -10,7 +10,7 @@ export default new DataSource({
   type: 'postgres',
   host: configService.get('DB_HOST'),
   port: configService.get('DB_PORT'),
-  username: configService.get('DB_USER'),
+  username: configService.get('DB_USERNAME'),
   password: configService.get('DB_PASSWORD'),
   database: configService.get('DB_DATABASE'),
   entities: [`${__dirname}/**/*.entity{.ts,.js}`],
